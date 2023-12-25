92284094582b2a91937bdb4605270adf6b05b01a
geon8692 <geon4735@gmail.com>
2023-12-25 16:04:23 +0900

    feat: spinner 기능 추가

--- scripts.js
+++ scripts.js
@@ -12,15 +12,17 @@ form.addEventListener('submit', e => {
 
 async function getGithub () {
     try{
+        document.getElementById('spinner').style.display='block';
         const response1 = await fetch(`https://api.github.com/users/${input.value}`);
         const user = await response1.json();
         const resposne2 = await fetch(`https://api.github.com/users/${input.value}/repos`);
         const repos = await resposne2.json();
         console.log(user);
         const ordedRepos = repos.sort((a, b) => new Date(a.created_at) - new Date(b.created_at)).reverse();
+        document.getElementById('spinner').style.display='none';
         ui.showResult(user, ordedRepos);
 
     } catch (e) {
-        console.error(e);
+        alert("This username does not exist.");
     }
 }
